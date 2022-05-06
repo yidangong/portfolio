ac713bebd7d46ea89600f4cd8b8c78edd21289d5
Yidan Gong <gyidangela@gmail.com>
2022-05-05 22:31:04 -0700

    Initial Commit from Template

---
+++ build/precache-manifest.9a119eb48f487e45fe62c0e7e7b304eb.js
@@ -0,0 +1,366 @@
+self.__precacheManifest = [
+  {
+    "revision": "b17b64b408648c733233",
+    "url": "/static/css/main.b5dd28b6.chunk.css"
+  },
+  {
+    "revision": "b17b64b408648c733233",
+    "url": "/static/js/main.59928734.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "125d3ec0d15479d01c6b",
+    "url": "/static/css/2.f8a7c81b.chunk.css"
+  },
+  {
+    "revision": "125d3ec0d15479d01c6b",
+    "url": "/static/js/2.1a7bd3a0.chunk.js"
+  },
+  {
+    "revision": "cdea8050eaa80b3b1e5db014670acb4c",
+    "url": "/static/media/pr3.cdea8050.jpg"
+  },
+  {
+    "revision": "6771771267548f6f558819406f00f92f",
+    "url": "/static/media/service.67717712.svg"
+  },
+  {
+    "revision": "45c3880f05b7bec5f4c87a80cd912fb1",
+    "url": "/static/media/6.45c3880f.png"
+  },
+  {
+    "revision": "a8390908ff9325f79e5c9c1950d66bc8",
+    "url": "/static/media/angular.a8390908.png"
+  },
+  {
+    "revision": "631927f8009b3c5cb56a5c6f4289734b",
+    "url": "/static/media/code.631927f8.png"
+  },
+  {
+    "revision": "cc583d9dcb32ac715883ee9b4e616d74",
+    "url": "/static/media/design-logo4.cc583d9d.png"
+  },
+  {
+    "revision": "107d9562fe90132ea9011259677053fb",
+    "url": "/static/media/man.107d9562.png"
+  },
+  {
+    "revision": "5208e89239d5db7b4069ef34129534e1",
+    "url": "/static/media/polygon.5208e892.png"
+  },
+  {
+    "revision": "93d86a7ddf3d4a821e551d4c59a67ecf",
+    "url": "/static/media/pr1.93d86a7d.jpg"
+  },
+  {
+    "revision": "f0d2c0cd230a0140ec80ad08c19d8df0",
+    "url": "/static/media/pr2.f0d2c0cd.jpg"
+  },
+  {
+    "revision": "1ff63cc59e39dfc9610eb5522ed7d534",
+    "url": "/static/media/react.1ff63cc5.png"
+  },
+  {
+    "revision": "b41f1706dc464f622f72aa8b2750fe7e",
+    "url": "/static/media/shap.b41f1706.png"
+  },
+  {
+    "revision": "4f64f3503956e955309785923828d2f6",
+    "url": "/static/media/video_shap.4f64f350.png"
+  },
+  {
+    "revision": "29e8b29e91876ecdaa493a90dbbf3b88",
+    "url": "/static/media/our-coaches-2.29e8b29e.jpg"
+  },
+  {
+    "revision": "66123bdac51dcc28983774ea013c96c1",
+    "url": "/static/media/our-coaches-7.66123bda.jpg"
+  },
+  {
+    "revision": "274736930e69ba244271a59b178ea3fc",
+    "url": "/static/media/portfolio1.27473693.jpg"
+  },
+  {
+    "revision": "9d682e6d0e6b7a53bb850ac11b4d07bb",
+    "url": "/static/media/slider2.9d682e6d.png"
+  },
+  {
+    "revision": "edc21cc301f200add64048a3b9f13b55",
+    "url": "/static/media/1.edc21cc3.png"
+  },
+  {
+    "revision": "e12a90a1b9ff09b17dbc289dd8ac2db3",
+    "url": "/static/media/2.e12a90a1.png"
+  },
+  {
+    "revision": "a9a320a15f27159728c1eba0a27550b1",
+    "url": "/static/media/3.a9a320a1.png"
+  },
+  {
+    "revision": "a5ee786ea51f5d513c08fbccac8dc97d",
+    "url": "/static/media/4.a5ee786e.png"
+  },
+  {
+    "revision": "204f5029996a25404adad45cde2515bf",
+    "url": "/static/media/5.204f5029.png"
+  },
+  {
+    "revision": "0f15c0025be13f5041c37108082931ab",
+    "url": "/static/media/apporoach_man_img.0f15c002.png"
+  },
+  {
+    "revision": "d852f421924ad748c564a9271b4feda4",
+    "url": "/static/media/banner_img.d852f421.svg"
+  },
+  {
+    "revision": "53982e8020fbfde7a8e67db84080705f",
+    "url": "/static/media/contact.53982e80.svg"
+  },
+  {
+    "revision": "0c7fc3d93d34e787693ac429b95d849f",
+    "url": "/static/media/1009269.0c7fc3d9.jpg"
+  },
+  {
+    "revision": "f25a0bd875999ee6899751b63e6dcdb5",
+    "url": "/static/media/1302261.f25a0bd8.jpg"
+  },
+  {
+    "revision": "5307e75c4672f43a0b8cbf92fe4dc568",
+    "url": "/static/media/1323866.5307e75c.jpg"
+  },
+  {
+    "revision": "20b27179289396311adfb259a7b510eb",
+    "url": "/static/media/f_man.20b27179.png"
+  },
+  {
+    "revision": "b28baa8958365c46aad2dde5d06371eb",
+    "url": "/static/media/intro-bg.b28baa89.jpg"
+  },
+  {
+    "revision": "d871640b32b141ebd58980f3bd4a483d",
+    "url": "/static/media/mobile.d871640b.svg"
+  },
+  {
+    "revision": "77982eeba0f6496de6da222e858f956a",
+    "url": "/static/media/bottomshape.77982eeb.png"
+  },
+  {
+    "revision": "5bacc112e42e4564246f3f116ea2ecdc",
+    "url": "/static/media/line1.5bacc112.png"
+  },
+  {
+    "revision": "21d1f793a41514a90fff7f7f7d8a07d1",
+    "url": "/static/media/line2.21d1f793.png"
+  },
+  {
+    "revision": "46731903d57664679095ca08a06f9847",
+    "url": "/static/media/line3.46731903.png"
+  },
+  {
+    "revision": "553b9128fb21cc6f1b28b39fc7df4368",
+    "url": "/static/media/shape1.553b9128.png"
+  },
+  {
+    "revision": "ebbc984fe3ac4e0de14b7c868e90a6fa",
+    "url": "/static/media/shape2.ebbc984f.png"
+  },
+  {
+    "revision": "52fd2fadf2f356ddf582af2d95d1273e",
+    "url": "/static/media/slider_img1.52fd2fad.jpg"
+  },
+  {
+    "revision": "022f6d3876ea9b174f94ed8b17a1ee07",
+    "url": "/static/media/slider_img2.022f6d38.jpg"
+  },
+  {
+    "revision": "6c06d376db51db01d74d3ad9ba597b75",
+    "url": "/static/media/slider_img3.6c06d376.jpg"
+  },
+  {
+    "revision": "82424752c6956283a9bc151597c6e918",
+    "url": "/static/media/topshape2.82424752.png"
+  },
+  {
+    "revision": "b3c89bef000b4dc747c402f126b981af",
+    "url": "/static/media/new1.b3c89bef.jpg"
+  },
+  {
+    "revision": "b4ed80d59ac6e550e40ee4d1b364e730",
+    "url": "/static/media/1.b4ed80d5.jpg"
+  },
+  {
+    "revision": "8f2d49becc923f24f3d0373b9b2f4f48",
+    "url": "/static/media/10.8f2d49be.jpg"
+  },
+  {
+    "revision": "8a7df260cbefa06836ec736724babef9",
+    "url": "/static/media/2.8a7df260.jpg"
+  },
+  {
+    "revision": "8799694ef20761b2ff98dd9c4855075c",
+    "url": "/static/media/3.8799694e.jpg"
+  },
+  {
+    "revision": "51daa7bea6551f9c11584abd4fa4324b",
+    "url": "/static/media/4.51daa7be.jpg"
+  },
+  {
+    "revision": "ba859a0af629796dc5a6ef6e4b711328",
+    "url": "/static/media/9.ba859a0a.jpg"
+  },
+  {
+    "revision": "b8328fd04335c47f207210c60132533c",
+    "url": "/static/media/img-3.b8328fd0.jpg"
+  },
+  {
+    "revision": "35b5680d41d9ddb941f01cd7a28986c8",
+    "url": "/static/media/portfolio2.35b5680d.jpg"
+  },
+  {
+    "revision": "8ca05271206b9b975227daee4ccbb464",
+    "url": "/static/media/profile.8ca05271.jpg"
+  },
+  {
+    "revision": "1add9ee7635e549c71644bbd95e8a1f9",
+    "url": "/static/media/shadow.1add9ee7.png"
+  },
+  {
+    "revision": "1040818bc81af3100b668bed958f6743",
+    "url": "/static/media/table.1040818b.png"
+  },
+  {
+    "revision": "bc1380d8422c6866764829435df302ab",
+    "url": "/static/media/testimonial.bc1380d8.svg"
+  },
+  {
+    "revision": "b40f85cbdcf06fbe1eec80a3d7988860",
+    "url": "/static/media/Sail-Away.b40f85cb.jpg"
+  },
+  {
+    "revision": "f6fa49681c1b53facb6b0299b639d957",
+    "url": "/static/media/agency-skill.f6fa4968.jpg"
+  },
+  {
+    "revision": "58a42f376447cd2d4e4c644d34838c94",
+    "url": "/static/media/blog-1.58a42f37.jpg"
+  },
+  {
+    "revision": "46593e522260cccafdb15eb5d906d41b",
+    "url": "/static/media/blog-2.46593e52.jpg"
+  },
+  {
+    "revision": "2c7628a1d335f4a533b21797b6d02c53",
+    "url": "/static/media/creative_bg1.2c7628a1.jpg"
+  },
+  {
+    "revision": "a721bb3fda897e18affe161b8abd0ebb",
+    "url": "/static/media/dot.a721bb3f.png"
+  },
+  {
+    "revision": "1a5b42ae895ad2ca4df9cae1a09a1c33",
+    "url": "/static/media/icebg.1a5b42ae.jpg"
+  },
+  {
+    "revision": "978fe695813ef87f502703086dfcbcad",
+    "url": "/static/media/img.978fe695.svg"
+  },
+  {
+    "revision": "1fc2d46583cf67106413cbd92769f85b",
+    "url": "/static/media/man.1fc2d465.jpg"
+  },
+  {
+    "revision": "4eaa84448f82149a5aa99445532b3603",
+    "url": "/static/media/line4.4eaa8444.png"
+  },
+  {
+    "revision": "4ee12d28987ae354bbd3aa83fdfdbb11",
+    "url": "/static/media/slider_img4.4ee12d28.jpg"
+  },
+  {
+    "revision": "40e08ec916e5b0c5072f70afd4afaf50",
+    "url": "/static/media/topshape.40e08ec9.png"
+  },
+  {
+    "revision": "f64ecd0c27709de3591d1849a21ee166",
+    "url": "/static/media/post-img1.f64ecd0c.jpg"
+  },
+  {
+    "revision": "f97e894c1a3198768218e46b0e071f3b",
+    "url": "/static/media/post-img2.f97e894c.jpg"
+  },
+  {
+    "revision": "6d3583ad2d20044ec4644ff6b5b46576",
+    "url": "/static/media/post-img3.6d3583ad.jpg"
+  },
+  {
+    "revision": "94de0520acb20dddd412634f5042c074",
+    "url": "/static/media/shap1.94de0520.png"
+  },
+  {
+    "revision": "918cbb425120d169dc84466a1a2abb2b",
+    "url": "/static/media/slider1.918cbb42.png"
+  },
+  {
+    "revision": "f9d179f59b0878ffcd32a5b3c8ae9c62",
+    "url": "/static/media/ElegantIcons.f9d179f5.ttf"
+  },
+  {
+    "revision": "fdd9e757bf61675343dcf55100422b84",
+    "url": "/static/media/ElegantIcons.fdd9e757.woff"
+  },
+  {
+    "revision": "d72ad3f702b9f23540e8ed78b4b65749",
+    "url": "/static/media/ElegantIcons.d72ad3f7.eot"
+  },
+  {
+    "revision": "907b74de12b89805612e69cd9ece33f6",
+    "url": "/static/media/ElegantIcons.907b74de.svg"
+  },
+  {
+    "revision": "b01ff252761958325faab1535c90c87f",
+    "url": "/static/media/et-line.b01ff252.woff"
+  },
+  {
+    "revision": "26ec3c7d0366e0825d705c6e224a8803",
+    "url": "/static/media/et-line.26ec3c7d.eot"
+  },
+  {
+    "revision": "98126e3e1238b0f3b941ad285320ce28",
+    "url": "/static/media/et-line.98126e3e.ttf"
+  },
+  {
+    "revision": "569bd9082c15cc30fa6e05626abdd505",
+    "url": "/static/media/et-line.569bd908.svg"
+  },
+  {
+    "revision": "ceeb894f4b74386e330043ca5c5d4d8f",
+    "url": "/static/media/Flaticon.ceeb894f.woff2"
+  },
+  {
+    "revision": "803ca4bc6a6f08a842a830bbb635f456",
+    "url": "/static/media/Flaticon.803ca4bc.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "943abd290ecac95ae6170ebded9711fb",
+    "url": "/index.html"
+  }
+];
