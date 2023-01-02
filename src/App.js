7bb266a98dc50828720abda5566f4d80cbe7aa5f
Yidan Gong <gyidangela@gmail.com>
2023-01-01 22:35:30 -0800

    Unbind unneeded pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 /*-----pages-----*/
-import { Home2 } from "./Home2";
 import { NotFound } from "./404";
-// import { CLP2 } from "./component/Projects/CLP2";
 import { HealthCheck } from "./HealthCheck";
 
 import { Home } from "./Home";
-import { Home1 } from "./Home1";
-import { Home3 } from "./Home3";
-import { Home4 } from "./Home4";
-import { Home5 } from "./Home5";
-import { Home6 } from "./Home6";
-import { Homefrelencer } from "./Home-frelencer";
-import { HomeDeveloper } from "./Home-developer";
+// import { Home1 } from "./Home1";
+// import { Home2 } from "./Home2";
+// import { Home3 } from "./Home3";
+// import { Home4 } from "./Home4";
+// import { Home5 } from "./Home5";
+// import { Home6 } from "./Home6";
+// import { Homefrelencer } from "./Home-frelencer";
+// import { HomeDeveloper } from "./Home-developer";
 
 import { CLP } from "./component/Projects/CLP";
 import { CLP2 } from "./component/Projects/CLP2";
@@ -27,16 +26,15 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/CLP" component={CLP} />
-            <Route path="/CLP2" component={CLP2} />
-            <Route path="/Home1" component={Home1} />
+
+            {/* <Route path="/Home1" component={Home1} />
             <Route path="/Home2" component={Home2} />
             <Route path="/Home3" component={Home3} />
             <Route path="/Home4" component={Home4} />
             <Route path="/Home5" component={Home5} />
             <Route path="/Home6" component={Home6} />
             <Route path="/Home-frelencer" component={Homefrelencer} />
-            <Route path="/Home-developer" component={HomeDeveloper} />
+            <Route path="/Home-developer" component={HomeDeveloper} /> */}
 
             <Route path="/Projects/2022201" component={CLP2} />
             <Route path="/Projects/2022101" component={CLP} />
